7e251c5368c0bc6cb8b10d3646785a8d3ab37f3e
Rayan Mahanna <rayan.mahanna@devinci.fr>
2020-12-16 15:51:06 +0100

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./App.css";
+import Profile from "../models/Profile";
+
+interface State extends Profile {
+}
+
+export class App extends React.Component<{}, State> {
+  state = {} as State
+
+  public componentDidMount() {
+    if (chrome && chrome.tabs) {
+      chrome.tabs.query({currentWindow: true, active: true}, tabs => {
+        const tab = tabs[0];
+        chrome.tabs.sendMessage(tab.id || 0, {from: "popup", subject: "getFullName"}, response => {
+          console.log(response);
+          this.setState({
+            fullName: response.fullName,
+            title: response.title,
+            country: response.country,
+            imageSrc: response.imageSrc
+          })
+        });
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <img src={this.state.imageSrc} alt={this.state.fullName}/>
+        <div className="fullName">{this.state.fullName}</div>
+        <div className="title">{this.state.title}</div>
+      </div>
+    );
+  }
+}
+
+export default App;
